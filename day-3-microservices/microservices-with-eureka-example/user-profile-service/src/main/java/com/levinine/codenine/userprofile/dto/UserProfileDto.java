7777e6ddef19a01ebb2dfb6c114ee759b8bb688b
m.marinkovic <m.marinkovic@levi9.com>
2024-04-18 16:39:23 +0200

    add endpoints for finding user profile

---
+++ day-3-microservices/microservices-with-eureka-example/user-profile-service/src/main/java/com/levinine/codenine/userprofile/dto/UserProfileDto.java
@@ -0,0 +1,13 @@
+package com.levinine.codenine.userprofile.dto;
+
+import com.levinine.codenine.userprofile.entity.UserProfile;
+
+public record UserProfileDto(Long id, String firstName, String lastName, String address,
+                             String libraryCardId) {
+
+  public static UserProfileDto fromUserProfile(UserProfile userProfile) {
+    return new UserProfileDto(userProfile.getId(), userProfile.getFirstName(),
+        userProfile.getLastName(), userProfile.getAddress(), userProfile.getLibraryCardId());
+  }
+
+}
---
+++ day-3-microservices/microservices-with-eureka-example/user-profile-service/src/main/java/com/levinine/codenine/userprofile/exception/GlobalExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.levinine.codenine.userprofile.exception;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+  @ExceptionHandler(EntityNotFoundException.class)
+  public ResponseEntity<ErrorDetails> handleEntityNotFound(EntityNotFoundException ex) {
+
+    ErrorDetails errorDetails = ErrorDetails.builder()
+        .status(HttpStatus.NOT_FOUND)
+        .message(ex.getMessage())
+        .build();
+
+    return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+  }
+
+}
