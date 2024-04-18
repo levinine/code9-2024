80f12b2770915d122a3ad02df9f565361a759ab0
m.marinkovic <m.marinkovic@levi9.com>
2024-04-18 14:57:06 +0200

    user-profile-service initial commit

---
+++ day-3-microservices/microservices-with-eureka-example/user-profile-service/src/test/java/com/levinine/codenine/userprofile/UserProfileServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.levinine.codenine.userprofile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserProfileServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
