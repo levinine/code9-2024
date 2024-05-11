1d67e4275e2c136220412553b107b450924d4ba2
Aleksa Kolarski <26332788+AleksaKolarski@users.noreply.github.com>
2024-05-11 08:42:16 +0200

    Update Results.java

--- day-1-java/stream-api/src/practice/results/Results.java
+++ day-1-java/stream-api/src/practice/results/Results.java
@@ -15,105 +15,21 @@ public class Results {
     public static void main(String[] args) {
         var results = new Results();
 
-        System.out.println("-------------------1-------------------");
-        results.resultOne();
-        System.out.println("-------------------2-------------------");
-        results.resultTwo();
-        System.out.println("-------------------3-------------------");
-        results.resultThree();
-        System.out.println("-------------------4-------------------");
-        results.resultFour();
-        System.out.println("-------------------5-------------------");
-        results.resultFive();
-        System.out.println("-------------------6-------------------");
-        results.resultSix();
-        System.out.println("-------------------7-------------------");
-        results.resultSeven();
+        //System.out.println("-------------------1-------------------");
+        //results.resultOne();
+        //System.out.println("-------------------2-------------------");
+        //results.resultTwo();
+        //System.out.println("-------------------3-------------------");
+        //results.resultThree();
+        //System.out.println("-------------------4-------------------");
+        //results.resultFour();
+        //System.out.println("-------------------5-------------------");
+        //results.resultFive();
+        //System.out.println("-------------------6-------------------");
+        //results.resultSix();
+        //System.out.println("-------------------7-------------------");
+        //results.resultSeven();
     }
 
-    /*
-     * Print the count of empty Strings
-     */
-    public void resultOne() {
-        long count = simpleStrings
-                .stream()
-                .filter(x -> x.isEmpty())
-                .count();
-        System.out.println("The number of empty Strings: " +  count);
-    }
-
-    /*
-     * Print the new List without empty Strings
-     */
-    public void resultTwo() {
-        List<String> filtered = simpleStrings
-                .stream()
-                .filter(x -> !x.isEmpty())
-                .collect(Collectors.toList());
-        System.out.println("Filtered list: " + filtered);
-    }
-
-    /*
-     * Print all persons between the age of 22 and 45 sorted
-     */
-    public void resultThree() {
-        System.out.println("Persons above 22 and under 45 years old are:");
-
-        personList
-                .stream()
-                .filter(person -> person.getAge() > 22 && person.getAge() < 45)
-                .sorted(Comparator.comparingInt(Person::getAge))
-                .forEach(person -> System.out.println(person.getName() + " age " + person.getAge()));
-    }
-
-    /*
-     * Print the average age of persons that live in Novi Sad
-     */
-    public void resultFour() {
-        personList
-                .stream()
-                .filter(person -> person.getCity().equals("Novi Sad"))
-                .mapToDouble(Person::getAge)
-                .average()
-                .ifPresent(i -> System.out.println("Average age is: " + i));
-    }
-
-    /*
-     * Group the persons by city, print the city and persons that live in it
-     */
-    public void resultFive() {
-        personList.stream()
-                .collect(Collectors.groupingBy(Person::getCity))
-                .forEach((city, peopleInCity) -> {
-                    System.out.println("City: " + city);
-                    peopleInCity.forEach(person -> System.out.println("  " + person.getName() + " " + person.getSurname()));
-                });
-    }
-
-    /* List all the cities persons are from */
-    public void resultSix() {
-        personList.stream()
-                .map(Person::getCity)
-                .distinct()
-                .sorted()
-                .forEach(System.out::println);
-    }
-
-    /*
-     * Move exactly one person from these cities
-     * ["Novi Sad", "Beograd", "Subotica"]
-     * to Kragujevac and print the result
-     */
-    public void resultSeven() {
-        Stream.of("Novi Sad", "Beograd", "Subotica")
-                .forEach(city -> personList.stream()
-                                .filter(person -> city.equals(person.getCity()))
-                                .findAny()
-                                .map(person -> {
-                                    person.setCity("Kragujevac");
-                                    return person;
-                                })
-                                .ifPresent(System.out::println)
-                        );
-    }
+    
 }
